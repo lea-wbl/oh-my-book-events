39f7c8461ca717af68a3a9755055d974d6d10158
lea-weibel <lea.weibel00@gmail.com>
2025-04-22 14:09:36 +0200

    deleted daisyui

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -39,5 +39,5 @@ export default {
       },
     },
   },
-  plugins: [require("daisyui"), require("tailwind-scrollbar-hide")],
+  plugins: [require("tailwind-scrollbar-hide")],
 } satisfies Config;
